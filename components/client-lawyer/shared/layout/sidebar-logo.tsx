0d9ce4698c588a7c73e48a2391476fc6863eb76d
sami-ramadan <samiramadan389@gmail.com>
2026-08-01 17:36:55 +0300

    feat: update SidebarLogo component to use async translations; add Arabic translations for client sidebar

--- components/client-lawyer/shared/layout/sidebar-logo.tsx
+++ components/client-lawyer/shared/layout/sidebar-logo.tsx
@@ -1,15 +1,21 @@
 import { Link } from "@/i18n/navigation";
 import PrimaryLogo from "@/components/icons/primary-logo";
-import { useTranslations } from "next-intl";
+import { getLocale, getTranslations } from "next-intl/server";
+import { cn } from "@/lib/utils";
 
-export default function SidebarLogo() {
-  const t = useTranslations("Header");
+export default async function SidebarLogo() {
+  const t = await getTranslations("Header");
+  const locale = await getLocale();
 
   return (
     <Link href="/" className="w-full">
       <div className="px-4 h-14 border-b border-secondary/20 flex items-center">
         <PrimaryLogo />
-        <span className="uppercase font-semibold font-lora ms-1">
+        <span
+          className={cn("uppercase font-semibold ms-1", {
+            "font-lora": locale === "en",
+          })}
+        >
           {t("Title")}
         </span>
       </div>
