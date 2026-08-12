9e3dc88739121ddf17b3a54a0084cddd6fc4138a
sami-ramadan <samiramadan389@gmail.com>
2026-08-12 19:21:24 +0300

    feat: remove image optimization settings from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 
