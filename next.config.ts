391fa4821ee1a7951449514299bc5a06a86091f0
sami-ramadan <samiramadan389@gmail.com>
2026-08-12 19:14:43 +0300

    feat: configure Next.js with image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 
