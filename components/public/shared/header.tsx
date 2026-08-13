bbc9803bd02ef967032ec180d2662c9f68c996a3
sami-ramadan <samiramadan389@gmail.com>
2026-08-13 12:19:56 +0300

    feat: enhance Header component with user authentication and dropdown menu

--- components/public/shared/header.tsx
+++ components/public/shared/header.tsx
@@ -8,11 +8,41 @@ import { navigationItems } from "@/constants/shared";
 import HeaderContainer from "./header/header-container";
 import LanguageSwitcher from "./header/language-switcher";
 import { getLocale, getTranslations } from "next-intl/server";
+import { http } from "@/lib/http";
+import { User } from "@/types/shared";
+import { cookies } from "next/headers";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  ChevronDown,
+  LayoutDashboardIcon,
+  LogOutIcon,
+  Settings,
+  UserIcon,
+} from "lucide-react";
 
 export default async function Header() {
-  const t = await getTranslations("Header");
   const locale = await getLocale();
-  const navItems = navigationItems(t);
+  const t = await getTranslations("Header");
+  const cookiesStore = await cookies();
+  const token = cookiesStore.get("token")?.value;
+  let user: User | null = null;
+
+  if (token) {
+    const { data, ok } = await http.get<{ data: User }>("/api/auth/me");
+
+    if (!ok) {
+      throw new Error("Failed to fetch user data");
+    }
+
+    user = data.data;
+  }
 
   return (
     <HeaderContainer>
@@ -38,32 +68,89 @@ export default async function Header() {
           aria-label={t("NavigationLabel")}
           className="hidden items-center gap-9 lg:flex"
         >
-          {navItems.map((item) => {
+          {navigationItems(t).map((item) => {
             return (
               <NavLink key={item.href} href={item.href} label={item.label} />
             );
           })}
         </nav>
 
-        <div className="hidden items-center gap-7 lg:flex">
-          <LanguageSwitcher />
+        {!user && (
+          <div className="hidden items-center gap-7 lg:flex">
+            <LanguageSwitcher />
 
-          <Link
-            href="/sign-in"
-            className="text-sm font-semibold text-white/65 transition-colors hover:text-white"
-          >
-            {t("SignIn")}
-          </Link>
-
-          <Link href="/get-started">
-            <Button
-              variant="outline"
-              className="bg-transparent rounded-xs h-9.5 w-30.25 border-secondary text-secondary hover:bg-secondary hover:text-primary"
+            <Link
+              href="/sign-in"
+              className="text-sm font-semibold text-white/65 transition-colors hover:text-white"
             >
-              {t("GetStarted")}
-            </Button>
-          </Link>
-        </div>
+              {t("SignIn")}
+            </Link>
+
+            <Link href="/get-started">
+              <Button
+                variant="outline"
+                className="bg-transparent rounded-xs h-9.5 w-30.25 border-secondary text-secondary hover:bg-secondary hover:text-primary"
+              >
+                {t("GetStarted")}
+              </Button>
+            </Link>
+          </div>
+        )}
+
+        {user && (
+          <div className="gap-3 hidden items-center lg:flex">
+            <LanguageSwitcher />
+
+            <DropdownMenu modal={false}>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="rounded-sm h-10 hover:bg-transparent hover:text-white"
+                >
+                  <Avatar className="w-8 h-8 me-1">
+                    <AvatarImage src={user.photo_url || ""} />
+                    <AvatarFallback>
+                      {user.first_name.slice(0, 2).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <span className="font-medium">{user.first_name}</span>
+                  <ChevronDown className="size-4" />
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent
+                className="w-56 rounded-xs shadow-none border-none! bg-white p-0"
+                align="end"
+                forceMount
+              >
+                <div className="px-4 py-2">
+                  <p className="font-medium text-primary text-sm">
+                    {user.first_name} {user.last_name}
+                  </p>
+                  <span className="text-primary/40 text-xs">{user.email}</span>
+                </div>
+                <DropdownMenuSeparator className="bg-secondary/20" />
+                <DropdownMenuItem className="h-10 rounded-none cursor-pointer px-4 text-secondary hover:bg-secondary/10!">
+                  <LayoutDashboardIcon className="size-4 text-secondary" />
+                  {t("GoToDashboard")}
+                </DropdownMenuItem>
+                <DropdownMenuItem className="h-10 rounded-none cursor-pointer px-4 text-primary hover:bg-secondary/10!">
+                  <UserIcon className="size-4 text-primary/40" />
+                  {t("Profile")}
+                </DropdownMenuItem>
+                <DropdownMenuItem className="h-10 rounded-none cursor-pointer px-4 text-primary hover:bg-secondary/10!">
+                  <Settings className="size-4 text-primary/40" />
+                  {t("Settings")}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="bg-secondary/20" />
+                <DropdownMenuItem className="h-10 rounded-none cursor-pointer px-4 text-[#9b2c2c]">
+                  <LogOutIcon className="text-[#9b2c2c]" />
+                  {t("Logout")}
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        )}
 
         <MobileMenu />
       </div>
--- types/shared.ts
+++ types/shared.ts
@@ -13,3 +13,21 @@ export type AuthFormActionsResponse<T> =
       success: false;
       errors?: Partial<Record<keyof T, string>>;
     };
+
+export type User = {
+  city: string;
+  country: string;
+  created_at: string;
+  email: string;
+  email_verified: boolean;
+  first_name: string;
+  id: number;
+  last_name: string;
+  lawyer_profile: null;
+  name: string;
+  phone: string;
+  phone_verified: boolean;
+  photo_url: string | null;
+  role: GuestType;
+  status: "active" | "inactive";
+};
