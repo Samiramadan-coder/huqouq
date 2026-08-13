f47b1e2ce2cd3e981325f415fd261506a8b09cf9
sami-ramadan <samiramadan389@gmail.com>
2026-08-13 11:42:44 +0300

    feat: implement token check in RootLayout to redirect authenticated users

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -1,8 +1,17 @@
-export default function RootLayout({
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (token) {
+    return redirect("/");
+  }
   return (
     <main className="min-h-screen grid place-content-center py-5">
       {children}
