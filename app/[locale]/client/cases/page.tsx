e83b83a8a701438f0a43e0e6e524e511a01854f8
sami-ramadan <samiramadan389@gmail.com>
2026-08-23 20:19:55 +0300

    feat: implement Cases page with dynamic localization and filters

--- app/[locale]/client/cases/page.tsx
+++ app/[locale]/client/cases/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <div>My Cases</div>;
+import { cn } from "@/lib/utils";
+import { getLocale, getTranslations } from "next-intl/server";
+
+export default async function Page() {
+  const locale = await getLocale();
+  const t = await getTranslations("Client.Cases");
+  const fontClass = locale === "en" ? "font-lora" : "";
+
+  return (
+    <div className="space-y-6">
+      <h1 className={cn("text-2xl font-semibold", fontClass)}>
+        {t("MyCases")}
+      </h1>
+
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
 }
