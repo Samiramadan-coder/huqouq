09491d79b3814c777cca4151c73ac5c616471b82
sami-ramadan <samiramadan389@gmail.com>
2026-09-06 21:31:51 +0300

    feat: update pending fees message for clarity and remove console logs from case details

--- components/client-lawyer/client/cases/details/lawyer-offer-card.tsx
+++ components/client-lawyer/client/cases/details/lawyer-offer-card.tsx
@@ -17,6 +17,7 @@ export default function LawyerOfferCard({
   caseId: number;
   caseOffer: CaseOffer;
 }) {
+  console.log("caseOffer", caseOffer);
   const locale = useLocale();
   const t = useTranslations("Client.Cases");
   const tCommon = useTranslations("Common");
