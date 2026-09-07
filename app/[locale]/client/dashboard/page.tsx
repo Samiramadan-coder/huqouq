41448bcb37dbdd4601debcc914e416fa9afc9499
sami-ramadan <samiramadan389@gmail.com>
2026-09-07 21:18:50 +0300

    feat: comment out unused components in dashboard page for cleaner code

--- app/[locale]/client/dashboard/page.tsx
+++ app/[locale]/client/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import { http } from "@/lib/http";
 import { CaseDetails } from "@/types/client/cases";
-import Stats from "@/components/client-lawyer/client/dashboard/stats";
+// import Stats from "@/components/client-lawyer/client/dashboard/stats";
 import WelcomeText from "@/components/client-lawyer/reusable/welcome-text";
-import NeedHelp from "@/components/client-lawyer/client/dashboard/need-help";
+// import NeedHelp from "@/components/client-lawyer/client/dashboard/need-help";
 import RecentCases from "@/components/client-lawyer/client/dashboard/recent-cases";
-import LegalServices from "@/components/client-lawyer/client/dashboard/legal-services";
+// import LegalServices from "@/components/client-lawyer/client/dashboard/legal-services";
 
 export default async function DashboardPage() {
   const { data, ok } = await http.get<{
@@ -18,9 +18,9 @@ export default async function DashboardPage() {
   return (
     <div className="container max-w-7xl space-y-6">
       <WelcomeText />
-      <Stats />
+      {/* <Stats />
       <NeedHelp />
-      <LegalServices />
+      <LegalServices /> */}
       <RecentCases cases={data.data} />
     </div>
   );
